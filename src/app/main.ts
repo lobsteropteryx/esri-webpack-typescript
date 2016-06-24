54843fb044afdb90b5f7d95cc9fd5afbb64bda75
David Stevens <david.stevens@eoleres.com>
2016-06-24 17:47:18 +0200

    Migrated to arcgis js v4.0

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,4 +1,12 @@
-import Map = require("esri/map");
+import * as Map from "esri/Map";
+import * as MapView from "esri/views/MapView";
 
-var map = new Map("content", {basemap: "streets"});
-
+var map = new Map({
+    basemap: "streets"
+});
+var view = new MapView({
+    container: "content",
+    map: map,
+    zoom: 4,
+    center: [0, 45]
+});
